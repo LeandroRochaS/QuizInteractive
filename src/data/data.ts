ef868f0da2739d0d94f5c58934e934464ee59dc9
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-26 22:20:17 -0300

    feat: create quests data

---
+++ src/data/data.ts
@@ -0,0 +1,24 @@
+const perguntas = [
+  {
+    pergunta: "Qual é a capital do Brasil?",
+    respostas: ["Rio de Janeiro", "São Paulo", "Brasília", "Salvador"],
+    respostaCorreta: "Brasília",
+  },
+  {
+    pergunta: "Quanto é 2 + 2?",
+    respostas: ["3", "4", "5", "6"],
+    respostaCorreta: "4",
+  },
+  {
+    pergunta: "Quem foi o primeiro presidente dos Estados Unidos?",
+    respostas: [
+      "George Washington",
+      "Thomas Jefferson",
+      "Abraham Lincoln",
+      "John Adams",
+    ],
+    respostaCorreta: "George Washington",
+  },
+];
+
+export const getAllPerguntas = () => perguntas;
