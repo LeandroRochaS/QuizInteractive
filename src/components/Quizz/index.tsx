dcb375eb032bf7e5b32a4c3ed713686a7dcbd69a
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-26 22:27:49 -0300

    feat: created type Perguntas and init states

--- src/components/Quizz/index.tsx
+++ src/components/Quizz/index.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import "./styles.scss";
+import { getAllPerguntas } from "../../data/data";
+
+type Pergunta = {
+  pergunta: string;
+  respostas: string[];
+  respostaCorreta: string;
+};
 
 export default function Quizz() {
+  const [perguntas, setPerguntas] = useState<Pergunta[]>([]);
+  const [perguntaAtual, setPerguntaAtual] = useState(0);
+  const [acertos, setAcertos] = useState(0);
+  const [erros, setErros] = useState(0);
+
+  useEffect(() => {
+    setPerguntas(getAllPerguntas());
+  }, [perguntaAtual, perguntas]);
+
   return (
     <>
       <h1>Quizz</h1>
