f884e5cdee77802e67c40931be1ccbf44c897d63
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-27 11:26:14 -0300

    feat: create login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import QuizzPage from "./pages/QuizPage";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/quizz/:assunto" element={<QuizzPage />} />
       </Routes>
     </>
