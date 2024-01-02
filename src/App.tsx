e2b61693c00a4ac742091aa1ea68c0e5c72a361d
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-02 11:55:51 -0300

    fix: remove login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import QuizzPage from "./pages/QuizPage";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/quizz/:assunto" element={<QuizzPage />} />
       </Routes>
     </>
