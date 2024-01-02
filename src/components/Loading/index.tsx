c5d5c011f00a8af8260b41c9c24d73b4e309f52d
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-01 21:39:56 -0300

    feat: add component loader

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+export const Loading = () => {
+  return (
+    <>
+      <div className="loader-container">
+        <div className="loader"></div>
+      </div>
+    </>
+  );
+};
