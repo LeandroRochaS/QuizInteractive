a888850dbcbed92fd636c25e1da7793d733aafc8
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-03 12:44:07 -0300

    feat: try change cursor

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,23 @@ import QuizzPage from "./pages/QuizPage";
 import Home from "./pages/Home";
 
 function App() {
+  const cursorDot = document.querySelector("[data-cursor-dot]");
+  const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+  window.addEventListener("mousemove", (e) => {
+    const { clientX, clientY } = e;
+
+    if (cursorDot instanceof HTMLElement) {
+      cursorDot.style.left = `${clientX}px`;
+      cursorDot.style.top = `${clientY}px`;
+
+      if (cursorOutline instanceof HTMLElement) {
+        cursorOutline.style.left = `${clientX}px`;
+        cursorOutline.style.top = `${clientY}px`;
+      }
+    }
+  });
+
   return (
     <>
       <Routes>
